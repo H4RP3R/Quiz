c69fd1d02abeb8362764c53289224f2e22f89643
zombiehunter <viktormallory25@gmail.com>
2024-12-06 13:51:27 +0300

    feat(quiz): add Topic() function

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,9 +2,12 @@ package quiz
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
+
+var ErrEmptyQuiz = fmt.Errorf("quiz does not contain any questions")
 
 // Quiz represents a collection of questions.
 type quiz struct {
@@ -14,6 +17,16 @@ type quiz struct {
 // Size returns the number of questions in the quiz.
 func (q *quiz) Size() int {
 	return len(q.Questions)
+}
+
+// Topic returns the topic of the first question in the quiz, which is also the
+// topic of all quiz. If the quiz is empty, it returns an error.
+func (q *quiz) Topic() (string, error) {
+	if q.Size() < 1 {
+		return "", ErrEmptyQuiz
+	}
+
+	return q.Questions[0].Topic, nil
 }
 
 // load loads the quiz from a JSON file at the given path.
