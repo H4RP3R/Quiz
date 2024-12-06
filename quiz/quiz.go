1409f7311e548c0ee17d1cf3ffbb17167438a1a7
zombiehunter <viktormallory25@gmail.com>
2024-12-06 17:25:56 +0300

    refactor(quiz): export Quiz type for external usage

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -10,18 +10,18 @@ import (
 var ErrEmptyQuiz = fmt.Errorf("quiz does not contain any questions")
 
 // Quiz represents a collection of questions.
-type quiz struct {
+type Quiz struct {
 	Questions []question
 }
 
 // Size returns the number of questions in the quiz.
-func (q *quiz) Size() int {
+func (q *Quiz) Size() int {
 	return len(q.Questions)
 }
 
 // Topic returns the topic of the first question in the quiz, which is also the
 // topic of all quiz. If the quiz is empty, it returns an error.
-func (q *quiz) Topic() (string, error) {
+func (q *Quiz) Topic() (string, error) {
 	if q.Size() < 1 {
 		return "", ErrEmptyQuiz
 	}
@@ -30,7 +30,7 @@ func (q *quiz) Topic() (string, error) {
 }
 
 // load loads the quiz from a JSON file at the given path.
-func (q *quiz) load(path string) {
+func (q *Quiz) load(path string) {
 	f, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -43,8 +43,8 @@ func (q *quiz) load(path string) {
 	}
 }
 
-func New(jsonPath string) *quiz {
-	q := quiz{}
+func New(jsonPath string) *Quiz {
+	q := Quiz{}
 	q.load(jsonPath)
 
 	return &q
